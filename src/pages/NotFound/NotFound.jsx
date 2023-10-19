80ada8f5f110601a32a1b9486d0cf3fff2cd98cd
PoliukhovychAnna <116736506+PoliukhovychAnna@users.noreply.github.com>
2023-10-19 22:21:24 +0300

    Update NotFound.jsx

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -3,7 +3,7 @@ import { Container, PageTitle } from './Styled.NotFound';
 const NotFound = () => {
   return (
     <Container>
-      <PageTitle>Sorry, this page doesn't exist'</PageTitle>
+      <PageTitle>Sorry, this page doesn't exist</PageTitle>
     </Container>
   );
 };
